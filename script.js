9fa98c9e21494e5ba19aeda2e6475e900a360b26
Leonardo Oliveira <leofelipoliv@icloud.com>
2024-08-24 22:27:54 -0300

    Update and coments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// A function mostrarLink() é ativada qaundo é clicado nos links da navbar, ao clicar na section selecionada,o funcionamento dela é, quando clicado na section, adiciona o "active" com o método do classList.add, e remove das outras com o laço forEach, com o CSS as sections que estão sem o active, ficam com o "display: none"
 function mostrarLink(id) {
   const section = window.document.querySelectorAll(".section");
   section.forEach((section) => {
